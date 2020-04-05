3120907a9257195ac16ea74ba87d4af7ffefac3e
Jeff Beyer <jeff.beyer@gmail.com>
2020-04-05 10:17:48 -0700

    adds scripts for psql

---
+++ dev/mac/run_psql.sh
@@ -0,0 +1,2 @@
+docker-compose run --rm postgres psql -h postgres -p 5432 -U postgres $@
+# docker exec -it pandaid-api_postgres_1 psql -U postgres
---
+++ dev/win/run_psql.bat
@@ -0,0 +1,2 @@
+docker-compose run --rm postgres psql -h postgres -p 5432 -U postgres %*
+REM docker exec -it pandaid-api_postgres_1 psql -U postgres
