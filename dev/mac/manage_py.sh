7e9d15d57105d59bc5a96ae95ebe341535116227
Jeff Beyer <jeff.beyer@gmail.com>
2020-04-05 11:44:00 -0700

    changed commands to docker syntax

--- dev/mac/manage_py.sh
+++ dev/mac/manage_py.sh
@@ -1,1 +1,2 @@
-docker-compose run --rm web python manage.py $@
+docker exec -it pandaid-api_web_1 python manage.py $@
+# docker-compose run --rm web python manage.py $@
--- dev/mac/run_psql.sh
+++ dev/mac/run_psql.sh
@@ -1,2 +1,2 @@
-docker-compose run --rm postgres psql -h postgres -p 5432 -U postgres $@
-# docker exec -it pandaid-api_postgres_1 psql -U postgres
+docker exec -it pandaid-api_postgres_1 psql -U postgres $@
+# docker-compose run --rm postgres psql -h postgres -p 5432 -U postgres $@
